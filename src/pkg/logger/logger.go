f6144e2399456cdf423fe68fb10a57d275bc6334
Aleksei Deviatovskii <mycunycu2607@gmail.com>
2021-05-13 23:48:08 +0300

    updated logger configuration

--- src/pkg/logger/logger.go
+++ src/pkg/logger/logger.go
@@ -1,26 +1,70 @@
 package logger
 
 import (
-	"carsales/internal/config"
+	"fmt"
+	"log"
+	"os"
+	"time"
 
 	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
 )
 
-type Logger struct {
-	*zap.Logger
+var logger *zap.Logger
+
+func init() {
+	path := createDirectory()
+	writer := getWriter(path)
+	encoder := getEncoder()
+
+	core := zapcore.NewCore(encoder, writer, zapcore.DebugLevel)
+	logger = zap.New(core, zap.AddCaller())
+
+	zap.ReplaceGlobals(logger)
 }
 
-func Get() Logger {
-	var zapLogger *zap.Logger
-	cfg := config.Get()
+func createDirectory() string {
+	var err error
 
-	if cfg.Env == config.PROD_ENV {
-		zapLogger, _ = zap.NewProduction()
-		zapLogger.Info("Will used PROD preset logger")
+	path, err := os.Getwd()
+	if err != nil {
+		log.Fatal("logger | createDirectory | Getwd | err: ", err)
 	}
 
-	zapLogger, _ = zap.NewDevelopment()
-	zapLogger.Info("Will used DEV preset logger")
+	_, err = os.Stat(fmt.Sprintf("%s/logs", path))
+	if os.IsNotExist(err) {
+		err = os.Mkdir("logs", os.ModePerm)
+	}
 
-	return Logger{zapLogger}
+	if err != nil {
+		log.Fatal("logger | createDirectory | Mkdir | err: ", err)
+	}
+
+	return path
+}
+
+func getWriter(path string) zapcore.WriteSyncer {
+	path = path + "/logs/all.log"
+
+	file, err := os.OpenFile(path, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		log.Fatal("logger | getWriter | OpenFile | err: ", err)
+	}
+
+	return zapcore.AddSync(file)
+}
+
+func getEncoder() zapcore.Encoder {
+	cfg := zap.NewProductionEncoderConfig()
+	cfg.EncodeTime = zapcore.TimeEncoder(func(t time.Time, enc zapcore.PrimitiveArrayEncoder) {
+		enc.AppendString(t.UTC().Format("2006-01-02T15:0405Z0700"))
+	})
+
+	cfg.EncodeLevel = zapcore.CapitalLevelEncoder
+
+	return zapcore.NewConsoleEncoder(cfg)
+}
+
+func Get() *zap.Logger {
+	return logger
 }
